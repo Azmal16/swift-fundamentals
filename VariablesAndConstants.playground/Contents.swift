092fb68c3c3f1365ad2c1c70d70862170d520cf4
Azmal Awasaf <aawsaf@mymedicalhub.com>
2022-12-15 18:53:24 +0600

    add more files

---
+++ VariablesAndConstants.playground/Contents.swift
@@ -0,0 +1,15 @@
+//variables
+var myVar = "Hello"
+myVar = "Lovely Universe"
+print(myVar)
+
+var myInt = 100
+print(myInt )
+
+//constants
+let myConst = "Hello Lovely Universe!"
+print(myConst)
+
+//Defining types
+let pi: Double = 3.1416
+print("Value of pi: \(pi)")
