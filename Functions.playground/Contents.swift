64e95d7d56ce88f1e0dc3e5d984bbe005d1e6887
Azmal Awasaf <aawsaf@mymedicalhub.com>
2022-12-21 15:40:06 +0600

    update function

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,21 @@
+//Functions and return types
+func myFuncOne(a:Int, b:Int = 10) -> Int{
+    return a+b
+}
+
+print(myFuncOne(a: 15, b: 50))
+print(myFuncOne(a: 15))
+
+//Different styles of parameter names
+func myFuncTwo(firstNum a:Int, sedondNum b:Int = 10) -> Int{
+    return a+b
+}
+
+print(myFuncTwo(firstNum: 100))
+
+//Keeping parameter name only inside function
+func myFuncThree(_ name: String) {
+    print("The 2022 world cup winner is: \(name)")
+}
+
+myFuncThree("Messi!")
