2bfba39caaa8cd71cb05d64b0f918ee9ac0a607b
Azmal Awasaf <aawsaf@mymedicalhub.com>
2023-01-13 14:57:50 +0600

    update ternary operator

--- TernaryOperators.playground/Contents.swift
+++ TernaryOperators.playground/Contents.swift
@@ -1,7 +1,14 @@
 // program to check pass or fail
-let marks = 60
+let marks = 30
 
 // use of ternary operator
 let result = (marks >= 40) ? "pass" : "fail"
 
 print("You " + result + " the exam")
+
+// program to check if a number is positive, zero, or negative
+let num = 7
+
+let resultNum = (num == 0) ? "Zero" : ((num > 0) ? "Positive" : "Negative")
+
+print("The number is \(resultNum).")
