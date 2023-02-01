4d9e138686ff9597b07e9c1128d2b6be4a63c882
Azmal Awasaf <aawsaf@mymedicalhub.com>
2023-02-01 20:04:08 +0600

    create for in loop file

---
+++ For-inLoops.playground/Contents.swift
@@ -0,0 +1,8 @@
+//This playground demonstrates the for-in loop in Swift
+
+// access items of an array
+let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+for planet in planets {
+    print(planet)
+}
