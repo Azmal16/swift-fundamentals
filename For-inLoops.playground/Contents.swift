20ef93aabf3932a726026aef77caf07064359b36
Azmal Awasaf <aawsaf@mymedicalhub.com>
2023-02-02 17:04:01 +0600

    update loop

--- For-inLoops.playground/Contents.swift
+++ For-inLoops.playground/Contents.swift
@@ -12,3 +12,13 @@ print("New planet list:")
 for planet in planets where planet != "Pluto"{
   print(planet)
 }
+
+// iterate in range from i = 1 to 1 = 5
+for i in 1...5 {
+    print(i)
+}
+
+//with stride function
+for i in stride(from: 1, to: 10, by: 3) {
+    print(i)
+}
