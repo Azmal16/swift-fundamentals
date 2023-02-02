dee97564a36f25ae72f9cf446ce4d1ddc8c45217
Azmal Awasaf <aawsaf@mymedicalhub.com>
2023-02-02 16:27:54 +0600

    update

--- For-inLoops.playground/Contents.swift
+++ For-inLoops.playground/Contents.swift
@@ -1,8 +1,14 @@
 //This playground demonstrates the for-in loop in Swift
 
 // access items of an array
-let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
 
 for planet in planets {
     print(planet)
+}
+
+print("New planet list:")
+// remove Pluto from planets array
+for planet in planets where planet != "Pluto"{
+  print(planet)
 }
