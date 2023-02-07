ab52394bccb481582d7d35ba5345490f42e3515c
Azmal Awasaf <aawsaf@mymedicalhub.com>
2023-02-07 20:35:26 +0600

    update

--- ClassVsStruct.playground/Contents.swift
+++ ClassVsStruct.playground/Contents.swift
@@ -1,21 +1,19 @@
 //This playground explains the difference between Swift Classes and Structures. This demonstration is inspired from a video by Sean Allen
 
-class CarClass {
+class Car {
     var manufactureYear: Int
     var manufacturer: String
     var color: String
-    var numberOfDoors: Int
     
-    init(manufactureYear: Int, manufacturer: String, color: String, numberOfDoors: Int) {
+    init(manufactureYear: Int, manufacturer: String, color: String) {
         self.manufactureYear = manufactureYear
         self.manufacturer = manufacturer
         self.color = color
-        self.numberOfDoors = numberOfDoors
     }
 }
 
 //Create instance of the CarClass
-var myCar = CarClass(manufactureYear: 2020, manufacturer: "BMW", color: "Red", numberOfDoors: 4)
+var myCar = Car(manufactureYear: 2020, manufacturer: "BMW", color: "Red")
 
 //Assign the value of myCar to a new variable
 var stolenCar = myCar
@@ -25,6 +23,26 @@ stolenCar.color = "White"
 
 //Check the color of the stolen car
 print("Color of the stolen car: \(stolenCar.color)")
+//Prints: Color of the stolen car: White
 
 //Look how it also changes the color of myCar
-print("Color of my Car after getting stolen using class: \(myCar.color)")
+print("Color of my Car: \(myCar.color)")
+//Prints: Color of my Car after getting stolen using class: White
+//Both mycar and stolen car points to the same object
+
+//Creating car Struct
+struct Bike {
+    var manufactureYear: Int
+    var manufacturer: String
+    var color: String
+}
+
+//Creating instances using same process
+var myBike = Bike(manufactureYear: 2012, manufacturer: "Yamaha", color: "Blue")
+var stolenBike = myBike
+
+stolenBike.color = "Black"
+
+print("Color of Stolen Bike: \(stolenBike.color)")
+print("Color of My Bike: \(myBike.color)")
+//These two bike variables are not pointing to the same object.
